6fd9ac96895ce396353fa020201378cf4f2bb3df
ylwester <sylwester.miecznikowski@gmail.com>
2022-06-15 23:22:40 +0200

    Merge pull request #2
    * feat(home-screen): add meals choice to home screen

--- src/screens/home-screen/index.tsx
+++ src/screens/home-screen/index.tsx
@@ -1,27 +1,48 @@
 import React from 'react'
 import DefaultText from '../../components/common/custom-text/index'
-import {
-  Button,
-  Flex,
-  Text,
-  useColorMode,
-  useColorModeValue
-} from 'native-base'
+import { Box, Flex, useColorModeValue } from 'native-base'
+import HomeHeader from '../../components/home-screen-header/index'
+import { FlatList } from 'react-native'
+import MealCardItem from './meal-card-item'
+
+const meals = [
+  {
+    name: 'Śniadanie',
+    pictureUrl: require('@assets/meals/breakfast.jpeg')
+  },
+  { name: 'Obiad', pictureUrl: require('@assets/meals/dinner.jpeg') },
+  { name: 'Lunch', pictureUrl: require('@assets/meals/lunch.jpeg') },
+  { name: 'Kolacja', pictureUrl: require('@assets/meals/latedinner.jpeg') }
+]
 
 const HomeScreen = () => {
-  const { toggleColorMode, colorMode } = useColorMode()
-
   return (
-    <Flex
-      bg={useColorModeValue('custom.0', 'custom.100')}
+    <Box
+      safeArea
+      display="flex"
       flex={1}
-      justify="center"
-      align="center">
-      <DefaultText fontSize={40}>Hello again</DefaultText>
-      <DefaultText>{colorMode}</DefaultText>
-      <Text>base text</Text>
-      <Button onPress={toggleColorMode}>Toggle</Button>
-    </Flex>
+      px={3}
+      width="full"
+      bg={useColorModeValue('custom.0', 'custom.100')}>
+      <Box>
+        <HomeHeader title="Cześć, Sylwo" />
+      </Box>
+      <Box pt={2}>
+        <DefaultText fontSize={18} fontWeight="semibold">
+          Wybierz posiłek
+        </DefaultText>
+        <Flex flexDir="row" w="100%" px={1}>
+          <FlatList
+            numColumns={2}
+            data={meals}
+            keyExtractor={(_, index) => index.toString()}
+            renderItem={({ item }) => (
+              <MealCardItem name={item.name} pictureUrl={item.pictureUrl} />
+            )}
+          />
+        </Flex>
+      </Box>
+    </Box>
   )
 }
 
